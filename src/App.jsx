3c208ae5e2268a28bae4db47809a1ee75db9d6d9
ivlopez03 <ivlopez.dev@gmail.com>
2024-06-10 14:13:41 -0700

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -6,35 +6,48 @@ function App() {
   
 
   return (
-    <div className='p-16 w-full h-screen '>
+    <div className='p-16 w-full h-screen   '>
      
       <div className=''>
         <span className='  p-3 rounded-md'>@Ivan</span>
       </div>
-      <div className='flex gap-10  flex-wrap'>
-        <div className='mt-20 flex-1'>
+      <div className='flex gap-10 flex-wrap'>
+        <div className='mt-20 flex-1 '>
           <p className='text-9xl'> Hi !</p>
           <p className='text-4xl'> I'm a Software Developer.</p>
           <p className='text-6xl py-1'>I enjoy building things.</p>
           <p className='text-4xl'>Hope to be an <span className='text-info'>Indie Hacker </span>someday.</p>
-        </div>
-        <div className='mt-20 p-10 flex-1 '>
-          <a href='https://marketplace.visualstudio.com/items?itemName=Redimvs.redim' target='_blank' className='flex gap-4 p-2 border border-white  rounded-xl hover:shadow-md hover:border-base-300  m-2 transition duration-300'>
-            <div>
-              <img src="/redim_logo.png" alt="redim" className='w-[104px] min-w-[104px]' />
+          <div className='flex text-[#acacac] mt-9'>
+          <div className='mr-5'>
+              <a href="https://github.com/ivlopez03" className='flex items-center gap-1 hover:text-primary' target='_blank'>
+                <FaGithub />
+                Github
+                </a>
             </div>
-            <div className='mt-2'>
+            <div className=''>
+              <a href="https://x.com/IvlopezDev" className='flex items-center gap-1 hover:text-primary' target='_blank'>
+                <FaTwitterSquare />
+                Twitter</a>
+            </div>
+        </div>
+        </div>
+        <div className='mt-20 flex-1 md:p-0 '>
+          <a href='https://marketplace.visualstudio.com/items?itemName=Redimvs.redim' target='_blank' className='flex gap-4 p-1 mb-2  border border-white  rounded-xl hover:shadow-md hover:border-base-300 transition duration-300'>
+            <div>
+              <img src="/redim_logo.png" alt="redim" className='w-[104px] min-w-[80px] ' />
+            </div>
+            <div className='mt-2  '>
               <p className='text-xl'>Redim Optimizer</p>
               <p>Redim is visual studio code extension to optimize images by using simple commands.</p>
             </div>
           </a>
 
-          <div className='flex gap-4 p-2 border border-white  rounded-xl hover:shadow-md hover:border-base-300  m-2 transition duration-300'>
+          <div className='flex gap-4 p-1 border border-white  rounded-xl hover:shadow-md hover:border-base-300  transition duration-300'>
             <div>
-              <img src="/icon.png" alt="redim" className='w-[104px] min-w-[104px]' />
+              <img src="/icon.png" alt="redim" className='w-[104px] min-w-[80px]' />
             </div>
-            <div className='mt-2'>
-              <p className='text-xl flex items-center gap-2'>tasqie <span className='bg-base-200 px-1 text-[11px] rounded-md '>Coming soon </span> </p>
+            <div className='mt-2 text-wrap '>
+              <p className='text-xl flex items-center gap-2'>tasqie <span className='bg-base-200 px-1 text-[11px] rounded-md text-nowrap '>Coming soon </span> </p>
               <p>tasqie is a cool application to boost your productivity and manage your personal projects.</p>
             </div>
           </div>
@@ -42,19 +55,9 @@ function App() {
         </div>
 
       </div>
-      <div className='absolute bottom-4 flex text-[#acacac]'>
-        <div className='mr-5'>
-            <a href="https://github.com/ivlopez03" className='flex items-center gap-1 hover:text-primary' target='_blank'>
-              <FaGithub />
-              Github
-              </a>
-          </div>
-          <div className=''>
-            <a href="https://x.com/IvlopezDev" className='flex items-center gap-1 hover:text-primary' target='_blank'>
-              <FaTwitterSquare />
-              Twitter</a>
-          </div>
-      </div>
+      
+
+      
       
     </div>
   )
